41babb7555cfbcac4c16773024dd2212038f4f30
Enes ASILLIOĞLU <enesasillioglu@users.noreply.github.com>
2018-07-04 08:59:49 +0300

    getNotificationChannel is not function error fix
    Android 8.0 MIUI 9/10  throws error

--- local-notifications.android.js
+++ local-notifications.android.js
@@ -102,13 +102,15 @@ LocalNotifications.schedule = function (arg) {
         if (android.os.Build.VERSION.SDK_INT >= 26) {
           var channelId = "myChannelId"; // package scoped, so no need to add it ourselves
           var notificationManager = context.getSystemService(android.content.Context.NOTIFICATION_SERVICE);
-          var notificationChannel = notificationManager.getNotificationChannel(channelId);
-          if (notificationChannel === null) {
-            // for 'importance' (expose one day as plugin property), see https://developer.android.com/reference/android/app/NotificationManager.html
-            notificationChannel = new android.app.NotificationChannel(channelId, options.channel, android.app.NotificationManager.IMPORTANCE_HIGH);
-            notificationManager.createNotificationChannel(notificationChannel);
+          if (notificationManager.getNotificationChannel) {
+            var notificationChannel = notificationManager.getNotificationChannel(channelId);
+            if (notificationChannel === null) {
+              // for 'importance' (expose one day as plugin property), see https://developer.android.com/reference/android/app/NotificationManager.html
+              notificationChannel = new android.app.NotificationChannel(channelId, options.channel, android.app.NotificationManager.IMPORTANCE_HIGH);
+              notificationManager.createNotificationChannel(notificationChannel);
+            }
+            builder.setChannelId(channelId);
           }
-          builder.setChannelId(channelId);
         }
 
         if (options.groupedMessages !== null && Array.isArray(options.groupedMessages)) {
