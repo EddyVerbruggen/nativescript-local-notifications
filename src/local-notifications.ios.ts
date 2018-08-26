e0af8113d22380c2ca0ba003d343dca4f943ea56
Dani Gámez Franco <gmzcodes@outlook.com>
2018-08-26 20:03:20 +0200

    Fixes a typo in a comment.

--- src/local-notifications.ios.ts
+++ src/local-notifications.ios.ts
@@ -54,7 +54,7 @@ export class LocalNotificationsImpl extends LocalNotificationsCommon implements 
   }
 
   private static guid() {
-    // Not the best, but will it will do. See https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+    // Not the best, but it will work. See https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
 
     const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 
