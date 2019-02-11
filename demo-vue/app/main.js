4bf7f12c26c9bb8304488b464604044f10b8a933
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-02-11 12:22:54 +0100

    Vue demo app error entry point not found #140

---
+++ demo-vue/app/main.js
@@ -0,0 +1,12 @@
+import Vue from 'nativescript-vue'
+import App from './components/App'
+
+require ("nativescript-local-notifications");
+
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = (TNS_ENV === 'production')
+
+
+new Vue({
+  render: h => h('frame', [h(App)])
+}).$start()
