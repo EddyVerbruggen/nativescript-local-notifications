75f7293b56ec280a5773dbf7c4960d14ae094735
ezlo-ionutrusu <ionut.rusu@ezlo.com>
2019-10-30 15:25:07 +0200

    fix/lint#2

--- src/local-notifications.android.ts
+++ src/local-notifications.android.ts
@@ -1,28 +1,24 @@
-import * as app from 'tns-core-modules/application';
-import * as utils from 'tns-core-modules/utils/utils';
+import * as app from "tns-core-modules/application";
+import * as utils from "tns-core-modules/utils/utils";
 import {
   LocalNotificationsApi,
   LocalNotificationsCommon,
   ReceivedNotification,
   ScheduleInterval,
   ScheduleOptions
-} from './local-notifications-common';
+} from "./local-notifications-common";
 
 declare const android, com, global: any;
 
-const NotificationManagerCompatPackageName = useAndroidX()
-  ? global.androidx.core.app
-  : android.support.v4.app;
+const NotificationManagerCompatPackageName = useAndroidX() ? global.androidx.core.app : android.support.v4.app;
 
-function useAndroidX() {
+function useAndroidX () {
   return global.androidx && global.androidx.appcompat;
 }
 
 (() => {
   const registerLifecycleEvents = () => {
-    com.telerik.localnotifications.LifecycleCallbacks.registerCallbacks(
-      app.android.nativeApp
-    );
+    com.telerik.localnotifications.LifecycleCallbacks.registerCallbacks(app.android.nativeApp);
   };
 
   // Hook on the application events
@@ -33,75 +29,47 @@ function useAndroidX() {
   }
 })();
 
-export class LocalNotificationsImpl extends LocalNotificationsCommon
-  implements LocalNotificationsApi {
-  private static IS_GTE_LOLLIPOP: boolean =
-    android.os.Build.VERSION.SDK_INT >= 21;
+export class LocalNotificationsImpl extends LocalNotificationsCommon implements LocalNotificationsApi {
+
+  private static IS_GTE_LOLLIPOP: boolean = android.os.Build.VERSION.SDK_INT >= 21;
 
   private static getInterval(interval: ScheduleInterval): number {
-    if (interval === 'second') {
+    if (interval === "second") {
       return 1000; // it's in ms
-    } else if (interval === 'minute') {
+    } else if (interval === "minute") {
       return android.app.AlarmManager.INTERVAL_FIFTEEN_MINUTES / 15;
-    } else if (interval === 'hour') {
+    } else if (interval === "hour") {
       return android.app.AlarmManager.INTERVAL_HOUR;
-    } else if (interval === 'day') {
+    } else if (interval === "day") {
       return android.app.AlarmManager.INTERVAL_DAY;
-    } else if (interval === 'week') {
+    } else if (interval === "week") {
       return android.app.AlarmManager.INTERVAL_DAY * 7;
-    } else if (interval === 'month') {
+    } else if (interval === "month") {
       return android.app.AlarmManager.INTERVAL_DAY * 31; // well that's almost accurate
-    } else if (interval === 'year') {
+    } else if (interval === "year") {
       return android.app.AlarmManager.INTERVAL_DAY * 365; // same here
     } else {
       return undefined;
     }
   }
 
-  private static getIcon(
-    context: any /* android.content.Context */,
-    resources: any,
-    iconLocation?: string
-  ): string {
+  private static getIcon(context: any /* android.content.Context */, resources: any, iconLocation?: string): string {
     const packageName: string = context.getApplicationInfo().packageName;
-    return (
-      (iconLocation &&
-        iconLocation.indexOf(utils.RESOURCE_PREFIX) === 0 &&
-        resources.getIdentifier(
-          iconLocation.substr(utils.RESOURCE_PREFIX.length),
-          'drawable',
-          packageName
-        )) ||
-      (LocalNotificationsImpl.IS_GTE_LOLLIPOP &&
-        resources.getIdentifier(
-          'ic_stat_notify_silhouette',
-          'drawable',
-          packageName
-        )) ||
-      resources.getIdentifier('ic_stat_notify', 'drawable', packageName) ||
-      context.getApplicationInfo().icon
-    );
+    return iconLocation
+        && iconLocation.indexOf(utils.RESOURCE_PREFIX) === 0
+        && resources.getIdentifier(iconLocation.substr(utils.RESOURCE_PREFIX.length), "drawable", packageName)
+        || (LocalNotificationsImpl.IS_GTE_LOLLIPOP && resources.getIdentifier("ic_stat_notify_silhouette", "drawable", packageName))
+        || resources.getIdentifier("ic_stat_notify", "drawable", packageName)
+        || context.getApplicationInfo().icon;
   }
 
   private static cancelById(id: number): void {
     const context = utils.ad.getApplicationContext();
-    const notificationIntent = new android.content.Intent(
-      context,
-      com.telerik.localnotifications.NotificationAlarmReceiver.class
-    ).setAction('' + id);
-    const pendingIntent = android.app.PendingIntent.getBroadcast(
-      context,
-      0,
-      notificationIntent,
-      0
-    );
-    const alarmManager = context.getSystemService(
-      android.content.Context.ALARM_SERVICE
-    );
+    const notificationIntent = new android.content.Intent(context, com.telerik.localnotifications.NotificationAlarmReceiver.class).setAction("" + id);
+    const pendingIntent = android.app.PendingIntent.getBroadcast(context, 0, notificationIntent, 0);
+    const alarmManager = context.getSystemService(android.content.Context.ALARM_SERVICE);
     alarmManager.cancel(pendingIntent);
-    const notificationManager = context.getSystemService(
-      android.content.Context.NOTIFICATION_SERVICE
-    );
+    const notificationManager = context.getSystemService(android.content.Context.NOTIFICATION_SERVICE);
     notificationManager.cancel(id);
 
     com.telerik.localnotifications.Store.remove(context, id);
@@ -112,12 +80,10 @@ export class LocalNotificationsImpl extends LocalNotificationsCommon
       try {
         // nothing to do on this platform
         const context = utils.ad.getApplicationContext();
-        const hasPermission = NotificationManagerCompatPackageName.NotificationManagerCompat.from(
-          context
-        ).areNotificationsEnabled();
+        const hasPermission=NotificationManagerCompatPackageName.NotificationManagerCompat.from(context).areNotificationsEnabled();
         resolve(hasPermission);
       } catch (ex) {
-        console.log('Error in LocalNotifications.hasPermission: ' + ex);
+        console.log("Error in LocalNotifications.hasPermission: " + ex);
         reject(ex);
       }
     });
@@ -129,53 +95,45 @@ export class LocalNotificationsImpl extends LocalNotificationsCommon
         // nothing to do on this platform
         resolve(true);
       } catch (ex) {
-        console.log('Error in LocalNotifications.requestPermission: ' + ex);
+        console.log("Error in LocalNotifications.requestPermission: " + ex);
         reject(ex);
       }
     });
   }
 
-  addOnMessageReceivedCallback(
-    onReceived: (data: ReceivedNotification) => void
-  ): Promise<any> {
+  addOnMessageReceivedCallback(onReceived: (data: ReceivedNotification) => void): Promise<any> {
     return new Promise((resolve, reject) => {
       try {
         // note that this is ONLY triggered when the user clicked the notification in the statusbar
         com.telerik.localnotifications.LocalNotificationsPlugin.setOnMessageReceivedCallback(
-          new com.telerik.localnotifications.LocalNotificationsPluginListener({
-            success: notification => {
-              onReceived(JSON.parse(notification));
-            }
-          })
+            new com.telerik.localnotifications.LocalNotificationsPluginListener({
+              success: notification => {
+                onReceived(JSON.parse(notification));
+              }
+            })
         );
         resolve();
       } catch (ex) {
-        console.log(
-          'Error in LocalNotifications.addOnMessageReceivedCallback: ' + ex
-        );
+        console.log("Error in LocalNotifications.addOnMessageReceivedCallback: " + ex);
         reject(ex);
       }
     });
   }
 
-  addOnMessageClearedCallback(
-    onReceived: (data: ReceivedNotification) => void
-  ): Promise<any> {
+  addOnMessageClearedCallback(onReceived: (data: ReceivedNotification) => void): Promise<any> {
     return new Promise((resolve, reject) => {
       try {
         // note that this is ONLY triggered when the user clicked the notification in the statusbar
         com.telerik.localnotifications.LocalNotificationsPlugin.setOnMessageClearedCallback(
-          new com.telerik.localnotifications.LocalNotificationsPluginListener({
-            success: notification => {
-              onReceived(JSON.parse(notification));
-            }
-          })
+            new com.telerik.localnotifications.LocalNotificationsPluginListener({
+              success: notification => {
+                onReceived(JSON.parse(notification));
+              }
+            })
         );
         resolve();
       } catch (ex) {
-        console.log(
-          'Error in LocalNotifications.addOnMessageClearedCallback: ' + ex
-        );
+        console.log("Error in LocalNotifications.addOnMessageClearedCallback: " + ex);
         reject(ex);
       }
     });
@@ -187,7 +145,7 @@ export class LocalNotificationsImpl extends LocalNotificationsCommon
         LocalNotificationsImpl.cancelById(id);
         resolve(true);
       } catch (ex) {
-        console.log('Error in LocalNotifications.cancel: ' + ex);
+        console.log("Error in LocalNotifications.cancel: " + ex);
         reject(ex);
       }
     });
@@ -205,22 +163,16 @@ export class LocalNotificationsImpl extends LocalNotificationsCommon
         //   console.log(">> < 26, StatusBarNotification[0]: " + new android.service.notification.StatusBarNotification[0]);
         // }
 
-        const keys: Array<
-          string
-        > = com.telerik.localnotifications.Store.getKeys(
-          utils.ad.getApplicationContext()
-        );
+        const keys: Array<string> = com.telerik.localnotifications.Store.getKeys(utils.ad.getApplicationContext());
 
         for (let i = 0; i < keys.length; i++) {
           LocalNotificationsImpl.cancelById(parseInt(keys[i]));
         }
 
-        NotificationManagerCompatPackageName.NotificationManagerCompat.from(
-          context
-        ).cancelAll();
+        NotificationManagerCompatPackageName.NotificationManagerCompat.from(context).cancelAll();
         resolve();
       } catch (ex) {
-        console.log('Error in LocalNotifications.cancelAll: ' + ex);
+        console.log("Error in LocalNotifications.cancelAll: " + ex);
         reject(ex);
       }
     });
@@ -229,11 +181,7 @@ export class LocalNotificationsImpl extends LocalNotificationsCommon
   getScheduledIds(): Promise<number[]> {
     return new Promise((resolve, reject) => {
       try {
-        const keys: Array<
-          string
-        > = com.telerik.localnotifications.Store.getKeys(
-          utils.ad.getApplicationContext()
-        );
+        const keys: Array<string> = com.telerik.localnotifications.Store.getKeys(utils.ad.getApplicationContext());
 
         const ids: number[] = [];
         for (let i = 0; i < keys.length; i++) {
@@ -242,7 +190,7 @@ export class LocalNotificationsImpl extends LocalNotificationsCommon
 
         resolve(ids);
       } catch (ex) {
-        console.log('Error in LocalNotifications.getScheduledIds: ' + ex);
+        console.log("Error in LocalNotifications.getScheduledIds: " + ex);
         reject(ex);
       }
     });
@@ -259,25 +207,18 @@ export class LocalNotificationsImpl extends LocalNotificationsCommon
         // the persisted options are exactly like the original ones.
 
         for (let n in scheduleOptions) {
-          const options = LocalNotificationsImpl.merge(
-            scheduleOptions[n],
-            LocalNotificationsImpl.defaults
-          );
+          const options = LocalNotificationsImpl.merge(scheduleOptions[n], LocalNotificationsImpl.defaults);
 
           options.icon = LocalNotificationsImpl.getIcon(
-            context,
-            resources,
-            (LocalNotificationsImpl.IS_GTE_LOLLIPOP &&
-              options.silhouetteIcon) ||
-              options.icon
+              context,
+              resources,
+              LocalNotificationsImpl.IS_GTE_LOLLIPOP && options.silhouetteIcon || options.icon
           );
 
           options.atTime = options.at ? options.at.getTime() : 0;
 
           // Used when restoring the notification after a reboot:
-          options.repeatInterval = LocalNotificationsImpl.getInterval(
-            options.interval
-          );
+          options.repeatInterval = LocalNotificationsImpl.getInterval(options.interval);
 
           if (options.color) {
             options.color = options.color.android;
@@ -290,16 +231,15 @@ export class LocalNotificationsImpl extends LocalNotificationsCommon
           LocalNotificationsImpl.ensureID(options);
 
           com.telerik.localnotifications.LocalNotificationsPlugin.scheduleNotification(
-            new org.json.JSONObject(JSON.stringify(options)),
-            context
-          );
+              new org.json.JSONObject(JSON.stringify(options)),
+              context);
 
           scheduledIds.push(options.id);
         }
 
         resolve(scheduledIds);
       } catch (ex) {
-        console.log('Error in LocalNotifications.schedule: ' + ex);
+        console.log("Error in LocalNotifications.schedule: " + ex);
         reject(ex);
       }
     });
